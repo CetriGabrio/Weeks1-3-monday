c80d90d245000f101ff5745d53cc287ffff0f4c2
Gabriele <gabriele.ilacqua@icloud.com>
2026-01-22 13:52:43 -0500

    Added comments to the script

--- Assets/Scripts/Assignment 1/Stars.cs
+++ Assets/Scripts/Assignment 1/Stars.cs
@@ -2,7 +2,11 @@ using UnityEngine;
 
 public class Stars : MonoBehaviour
 {
+    //This script handles the stars behavior
+    //I used animation curve and mathf.lerp to shrink and grow the size of stars between a min nd max value
+    //For more details check the math sheet in the planning doc
 
+    //The variables for the curve, like size and speed
     public AnimationCurve sizeCurve;
     public float minSize;
     public float maxSize;
@@ -18,6 +22,7 @@ public class Stars : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //This is the looping part of the animation so that it stars again once reaching the max value
         t += Time.deltaTime / speed;
 
         if (t >= 1f)
@@ -27,7 +32,9 @@ public class Stars : MonoBehaviour
 
         float y = sizeCurve.Evaluate(t);
 
+        //As mentioned before, I am using Lerp to simply shift the size between two values, a min size and a max size
         float size = Mathf.Lerp(minSize, maxSize, y);
+        //And I am updating the localScale of each star
         transform.localScale = Vector3.one * size;
     }
 }
