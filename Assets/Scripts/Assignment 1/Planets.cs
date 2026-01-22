b42bdf2866e764f6d91a8d1282a4a110d93848ab
Gabriele <gabriele.ilacqua@icloud.com>
2026-01-22 14:41:17 -0500

    Added comments and finalized the project

--- Assets/Scripts/Assignment 1/Planets.cs
+++ Assets/Scripts/Assignment 1/Planets.cs
@@ -2,6 +2,9 @@ using UnityEngine;
 
 public class Planets : MonoBehaviour
 {
+    //This script is for the 2 planets that move and bounce at the edge of the screen
+
+    //I serialized the speed variable to be able to edit it in the inspector directly for easier tuning
     [SerializeField]
     float speed = 1.0f;
 
@@ -14,14 +17,19 @@ public class Planets : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //This part handles the movement of the planets
+        //They move linearly to the right since the speed has a positive value
+        //In the inspector I set the speed of the second planet to a negative value for the movement in opposite direction
         Vector2 newPosition = transform.position;
         newPosition.x += 1 * speed * Time.deltaTime;
         transform.position = newPosition;
 
+        //The following part instead deals with the bouncing off the edges of the screen into the opposite direction
         Vector2 screenPos = Camera.main.WorldToScreenPoint(transform.position);
 
         if (screenPos.x < 0 || screenPos.x > Screen.width)
         {
+            //As mentioned before, negative speed equal opposite direction
             speed = speed * -1;
         }
     }
--- Assets/Scripts/Assignment 1/Randomizer.cs
+++ Assets/Scripts/Assignment 1/Randomizer.cs
@@ -2,6 +2,9 @@ using UnityEngine;
 
 public class Randomizer : MonoBehaviour
 {
+    //This script is for the alien that randomly changes position every few seconds
+
+    //These are the variables for time and position on the y-axis (which I hand-picked to stay on the grass)
     private float interval = 3f;
     private float timer = 0f;
 
@@ -17,13 +20,16 @@ public class Randomizer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //This part handles the timer, so when the timer is done, the alien changes position
         timer += Time.deltaTime;
 
         if (timer >= interval)
         {
+            //On the x-axis I decided to instead use the screen width so the alien is always on screen
             float minX = Camera.main.ScreenToWorldPoint(new Vector2(0, 0)).x;
             float maxX = Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, 0)).x;
 
+            //This handles the random position between the 4 different values I have set previously
             float x = Random.Range(minX, maxX);
             float y = Random.Range(minY, maxY);
 
